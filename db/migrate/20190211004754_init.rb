e69467bcd12975409ce390c926aa7708dc4ca074
takehanKosuke <kkoussuke530@icloud.com>
2019-02-11 10:11:27 +0900

    first create model article & comment

--- db/migrate/20190211004754_init.rb
+++ db/migrate/20190211004754_init.rb
@@ -1,4 +1,18 @@
 class Init < ActiveRecord::Migration[5.2]
   def change
+    create_table :articles do |t|
+      t.string :title, null: false
+      t.text :body, null: false
+
+      # t.integer :user_id, null: false
+      t.timestamps
+    end
+
+    create_table :comments do |t|
+      t.text :body, null: false
+
+      t.integer :article_id, null: false
+      t.timestamps
+    end
   end
 end
